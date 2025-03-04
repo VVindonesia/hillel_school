03d42d9e295e7cd2285a338efe178b4dec804ae7
VVindonesia <microlimitq@gmail.com>
2025-03-04 13:44:17 +0100

    updated

--- 3.3.py
+++ 3.3.py
@@ -3,8 +3,10 @@ if len(list1) == 0:
     print(list1)
     print(list1)
 else:
-    list_first_half = list1[::2]
-    list_second_half = list1[1::2]
+    list_first_half = len(list1) //2
+    if len(list1) %2 != 0 :
+        list_first_half += 1
+    list_second_half = [list1[:list_first_half], list1[list_first_half:]]
     print(list_first_half)
     print(list_second_half)
-  #оно, вроде, соотвествует поставленному заданию, но я могу переписать, если вы сочтете решение некорректным   
+  #исправленный код, не обратил внимание на условия
